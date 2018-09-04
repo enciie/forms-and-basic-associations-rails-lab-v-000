9a52b1c86a0053e72f84c49bc05b11f119f42979
Nancy Jo <enciie@gmail.com>
2018-09-04 07:10:37 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -20,9 +20,15 @@ class Song < ActiveRecord::Base
   end
 
   def note_contents=(notes)
+    notes.each do |content|
+      if content.strip != ''
+        self.notes.build(content: content)
+      end
+    end
   end
 
   def note_contents
+    self.notes.map(&:content)
   end
 
 end
